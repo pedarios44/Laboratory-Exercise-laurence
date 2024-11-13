fd7a3437bed4bbf8979986d6bc0bd10a76dc14b2
pedarios44 <pedarioscaptain725@gmail.com>
2024-11-13 09:30:02 +0800

    Add files via upload

---
+++ LaboratoryExercise.java
@@ -0,0 +1,23 @@
+
+package laboratoryexercise;
+
+
+public class LaboratoryExercise {
+
+    public static void main(String[] args) {
+        int iNumber = 3;
+        
+        if (iNumber >= 0){
+            if(iNumber >=0){
+           System.out.println("first String");
+        }
+        
+        else{
+            System.out.println("Second String");
+        }
+        {
+            System.out.println("Third String");
+        }
+        }
+    }}
+      
